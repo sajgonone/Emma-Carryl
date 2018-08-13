0f2beefbf30a39352f73dfbcdb0b908933641415
Mariusz <dammariusz@o2.pl>
2018-08-13 21:15:54 +0100

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/homepage'
   devise_for :users
   devise_for :installs
   resources :links
+  
+  
+  root to:"welcome#homepage"
   
   root to:"links#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
