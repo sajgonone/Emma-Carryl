67e9052e9c996d7d1c394780d89684f3dc8cfde3
Mariusz <dammariusz@o2.pl>
2018-08-19 21:39:06 +0100

    mailers

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,8 @@
-class ContactMailer < ApplicationMailer
+class ContactMailer < ActionMailer::Base
+    default from: "from@example.com"
+
+    def contact_mailer
+
+        mail to: "mariuszdam@yahoo.com", subject: "Contact Me"
+    end
 end
