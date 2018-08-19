0eeb7e734368246b6ec7e8ec3197f33d2e8065f3
Mariusz <dammariusz@o2.pl>
2018-08-19 17:02:40 +0100

    homepage.html.erb

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
         firstName = name.split(' ').slice(0, -1).join(' ');
       }
       $this = $("#sendMessageButton");
-      $this.prop("disabled", false); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
         url: "././mail/contact_me.php",
         type: "POST",
@@ -28,7 +28,7 @@ $(function() {
           email: email,
           message: message
         },
-        cache: true,
+        cache: false,
         success: function() {
           // Success message
           $('#success').html("<div class='alert alert-success'>");
