9ac2492acb2a42964e89494ce568dbbd551d763e
Mariusz <dammariusz@o2.pl>
2018-08-19 21:15:14 +0100

    mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   
-  mail to: "mariuszdam@yahoo.com" subject: "$name"
+  mail to: "mariuszdam@yahoo.com"
   layout 'mailer'
 end
 
