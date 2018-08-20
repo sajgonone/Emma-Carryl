a10e23838c3c5b849a055a8d5982a56a4bf416c9
Mariusz <dammariusz@o2.pl>
2018-08-21 00:43:35 +0100

    config

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/homepage'
+  resources: contact_mailer
+  resources: contact_me
   devise_for :users
   devise_for :installs
   
-  
+  get 'welcome/homepage'
   root to:"welcome#homepage"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
