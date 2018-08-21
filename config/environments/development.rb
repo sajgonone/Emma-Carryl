c3876c3dee10e590b81d8528a529d34a69e8de23
Mariusz <dammariusz@o2.pl>
2018-08-21 19:44:58 +0100

    app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,18 +52,6 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_setting = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com"
-    authentication: :plain,
-    enable_starttls_auto: true,
-    user_name: string[truesajgon@gmail.com]
-    password: string[jeremiasz1]
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
